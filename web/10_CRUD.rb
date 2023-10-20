d3ebd514533a7330057682a835a233784d42d337
krillan <pushkinidze49@gmail.com>
2023-10-20 12:49:27 +0300

    l3 part1 kruc

--- web/10_CRUD.rb
+++ web/10_CRUD.rb
@@ -1,6 +1,6 @@
 puts '                                            CRUD(ActiveRecord)'
 
-# CRUD(основыные операции):
+# CRUD - аббревиатура обозначающая основыные операции:
 # Create - (new)              .create; .new.save
 # Read   - (index | show)     .where; .find(3); .all
 # Update - (update)
