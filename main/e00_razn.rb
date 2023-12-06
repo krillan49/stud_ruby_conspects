4929aca88ff7706661983628b48d5c4d93a4186a
krillan <pushkinidze49@gmail.com>
2023-12-06 20:05:06 +0300

    mhgfmhgfg

--- main/e00_razn.rb
+++ main/e00_razn.rb
@@ -24,6 +24,36 @@ p str1.split('') #=> ["a", "a", "a"]
 p str2.split('') #=> undefined method `split' for nil:NilClass (NoMethodError)
 p str2&.split('') #=> nil
 
+# Это triple equals. case/when так сравнивает например
+(97..122) === 100 #=> true
+
+# класс внутри класса методы в строку с =
+class Lottery
+
+  def initialize(**options)
+    @limited_of_tickets = options[:limited_of_tickets] || 5
+    @ticket_size = options[:ticket_size] || 3
+    @all_tickets = Array.new(@limited_of_tickets) { Ticket.new(@ticket_size).list }
+  end
+
+  def all_tickets = @all_tickets || []
+
+  class Ticket
+    include Comparable
+
+    def initialize(size)
+      @size = size
+      @all = Array.new(@size) { rand(99) }
+    end
+
+    def list = @all
+  end
+
+end
+
+lottery_1 =  Lottery.new(limited_of_tickets: 100,ticket_size: 3)
+pp lottery_1.all_tickets
+
 
 require 'some' # require - требовать
 
