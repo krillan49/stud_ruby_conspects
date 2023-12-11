c5b9e244e62159e8d3db84f9c469c9db05f4f7b2
krillan <pushkinidze49@gmail.com>
2023-12-11 15:40:37 +0300

    kruk l10 16-50

--- web/Rails/app/controllers/concerns/authentication.rb
+++ web/Rails/app/controllers/concerns/authentication.rb
@@ -2,11 +2,23 @@ module Authentication
   extend ActiveSupport::Concern
 
   included do
-    # код из application_controller.rb
+
     private
 
     def current_user
       @current_user ||= User.find_by(id: session[:user_id]).decorate if session[:user_id].present?
+    end
+    # модифицируем метод и под куки
+    def current_user
+      if session[:user_id].present? # сперва прверяем в сессии
+        @current_user ||= User.find_by(id: session[:user_id]).decorate
+      elsif cookies.encrypted[:user_id].present? # если нет в сессии то проверяем в куки
+        user = User.find_by(id: cookies.encrypted[:user_id]) # получаем юзера по айди взятому из куки
+        if user&.remember_token_authenticated?(cookies.encrypted[:remember_token]) # используем метод проверки из модели и проверяем токен из куки с хэшированным токеном этого пользователя из БД
+          sign_in(user) # если все сходится можно и сразу установить сессиию, чтобы ускорить послед проверки
+          @current_user ||= user&.decorate # и если все сходится назначаем текущего пользователя
+        end
+      end
     end
 
     def user_signed_in?
@@ -17,7 +29,22 @@ module Authentication
       session[:user_id] = user.id
     end
 
+    def remember(user) # метод запоминания пользователя в куки
+      user.remember_me # применяем метод из модели User заполняющий колонку remember_token_digest хэшем токена
+      cookies.encrypted.permanent[:remember_token] = user.remember_token # помещаем remember_token (не хэшированныый), который сгенерили в методе remember_me, в куки браузера пользователя, под ключем :remember_token
+      # permanent - необязательный метод, делающий куки практически вечным(тк по умолчанию срок действия куки истекает), но при желании можно поставить свой срок истечения https://api.rubyonrails.org/classes/ActionDispatch/Cookies.html
+      # encrypted - необязательный метод шифрующий куки(от пользователя), будет расшифровано автоматически на сервере
+      cookies.encrypted.permanent[:user_id] = user.id # поместим в куки и юзерайди, удобно для определения пользователя
+    end
+
+    def forget(user) # метод забывания/удаления токена и айди юзера из куки
+      user.forget_me # применяем метод из модели User удаляющий из колонки remember_token_digest хэш токена
+      cookies.delete :user_id
+      cookies.delete :remember_token
+    end
+
     def sign_out # метод для удаления сессии
+      forget(current_user) # заодно удалим куки(можно либо тут либо в экшене sessions#destroy)
       session.delete :user_id
       @current_user = nil
     end
