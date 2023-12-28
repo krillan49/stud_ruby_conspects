3267cb5739f39fdbc2d78c5afc8f6c0a0466f89d
krillan <pushkinidze49@gmail.com>
2023-12-28 16:55:21 +0300

    dhdhdgfdfg

--- 1zadachniky/codewars_2.rb
+++ 1zadachniky/codewars_2.rb
@@ -2,30 +2,53 @@
 # https://www.codewars.com/kata/584daf7215ac503d5a0001ae/train/ruby
 class DiffExp
   def initialize(exp)
+    # если не получится мб распарсить так ? : ["-", ["+", ["*", "x", 1], ["*", "x", 1]], ["*", "x", 1]]
     @exp = exp_to_arr(exp) # ["-", ["+", {"x"=>1}, {"x"=>1}], {"x"=>1}]
-    @oexp = ''
   end
 
   def main
     @exp = recursion_cycle(@exp) if @exp.class == Array
-    derivative
-    [@oexp, @exp]
+    @exp = derivative(@exp)
+    @exp = recursion_cycle(@exp) if @exp.class == Array
+    @exp
   end
 
-  def derivative # производная
-    if @exp[0] == 'sin'
-      @oexp = '(cos x)' # потом вставим упрощенное @exp вместо x
-      @exp = @exp[1]
-    elsif @exp[0] == 'cos'
-      @oexp = '(* -1 (sin x))'
-      @exp = @exp[1]
-    elsif @exp[0] == 'exp'
-      @oexp = '(exp x)'
-      @exp = @exp[1]
-    elsif @exp[0] == 'ln'
-      @oexp = '(/ 1 x)'
-      @exp = @exp[1]
+  def derivative(exp) # производная # потом вставим упрощенное @exp вместо x в oexp
+    # p exp
+    if exp[0] == 'sin' # sin(n*x) => n*cos(n*x)"
+      newexp = ['*', derivative(exp[1]), ['cos', exp[1]]]
+    elsif exp[0] == 'cos'
+      newexp = ['*', -1, ['*', derivative(exp[1]), ['sin', exp[1]]]]
+    elsif exp[0] == 'exp' # '(exp x)'
+      newexp = ['*', derivative(exp[1]), ['exp', exp[1]]]
+    elsif exp[0] == 'ln' # '(/ 1 x)'
+      p exp
+      newexp = ['/', 1, exp[1]]
+    elsif exp[0] == '^'
+      newexp = ["^", ["*", exp[2], exp[1]], exp[2] - 1]
+    elsif exp[0] == '/' # (u/v)' = (u'v - uv')/v^2
+      newexp = [
+        '/',
+        ['-', ['*', derivative(exp[1]), exp[2]], ['*', derivative(exp[2]), exp[1]]],
+        ['^', exp[2], 2]
+      ]
+    elsif exp[0] == '*'
+      if exp[1].class == Integer
+        exp[2] = exp[2].map{|e| e.class == Hash ? {'x' => e['x'] *= exp[1]} : e.class == Integer ? e * exp[1] : e} # * на коэф
+        newexp = derivative(exp[2]).compact
+      elsif exp[2].class == Integer
+        exp[1] = exp[1].map{|e| e.class == Hash ? {'x' => e['x'] *= exp[2]} : e.class == Integer ? e * exp[2] : e} # * на коэф
+        newexp = derivative(exp[1]).compact
+      end
+    elsif exp[0] == '+'
+      # newexp = "(+ #{exp[1]['x']}x #{exp[2]})"
+      newexp = ["+", derivative(exp[1]), derivative(exp[2])]
+    elsif exp.class == Hash # переменная и множитель
+      newexp = exp['x']
+    elsif exp.class == Integer
+      newexp = 0
     end
+    newexp
   end
 
   def recursion_cycle(exp) # упрощаем выражение
@@ -41,7 +64,26 @@ class DiffExp
 
   def simplify_inner_exp(exp) # упрощаем внутренний массив
     # p exp
-    if exp[1].class == Hash && exp[2].class == Hash && %w[+ -].include?(exp[0])  # оба с переменными
+    if exp[1].class == Integer && exp[2].class == Integer
+      exp[0] == '**' if exp[0] == '^'
+      exp[1].send(exp[0], exp[2])
+    elsif exp[0] == '+' && (exp[1] == 0 || exp[2] == 0)
+      exp[1] == 0 ? exp[2] : exp[1]
+    elsif exp[0] == '-' && (exp[1] == 0 || exp[2] == 0)
+      exp[1] == 0 ? -exp[2] : exp[1]
+    elsif exp[0] == '*' && (exp[1] == 0 || exp[2] == 0)
+      0
+    elsif exp[0] == '^'
+      if exp[1] == 0
+        0
+      elsif exp[2] == 0 || exp[1] == 1
+        1
+      elsif exp[2] == 1
+        exp[1]
+      else
+        exp
+      end
+    elsif exp[1].class == Hash && exp[2].class == Hash && %w[+ -].include?(exp[0])  # оба с переменными
       k = exp[1].keys[0]
       exp = {k => exp[1][k].send(exp[0], exp[2][k])}
     elsif exp[1..-1].any?{|e| e.class == Hash} && exp[1..-1].any?{|e| e.class != Hash} && %w[* /].include?(exp[0]) # одно с переменной
@@ -79,27 +121,74 @@ def diff(s)
   dif_exp.main
 end
 
-p diff("(cos x)") #
+# p diff('5')             # 0                     # constant should return 0"
+# p diff('x')             # 1                     # x should return 1"
+# p diff('(+ x x)')       # 2                     # x+x should return 2"
+# p diff('(- x x)')       # 0                     # x-x should return 0"
+# p diff('(* x 2)')       # 2                     # 2*x should return 2"
+# p diff('(/ x 2)')       # 0.5                   # x/2 should return 0.5"
+# p diff('(^ x 2)')       # (* 2 x)               # x^2 should return 2*x"
+# p diff('(+ x (+ x x))') # 3                     # x+(x+x) should return 3"
+# p diff('(- (+ x x) x)') # 1                     # (x+x)-x should return 1"
+# p diff('(* 2 (+ x 2))') # 2                     # 2*(x+2) should return 2"
+# p diff('(/ 2 (+ 1 x))') # (/ -2 (^ (+ 1 x) 2))  # 2/(1+x) should return -2/(1+x)^2"
+# p diff('(cos x)')       # (* -1 (sin x))        # cos(x) should return -1 * sin(x)"
+# p diff('(sin x)')       # (cos x)               # sin(x) should return cos(x)"
+# p diff('(exp x)')       # (exp x)               # exp(x) should return exp(x)"
+p diff('(ln x)')        # (/ 1 x)               # ln(x) should return 1/x"
+# p diff('(cos (+ x 1))') # (* -1 (sin (+ x 1)))  # cos(x+1) should return -1 * sin(x+1)"
+# p diff('(sin (+ x 1))') # (cos (+ x 1))         # sin(x+1) should return cos(x+1)"
+# p diff('(sin (* 2 x))') # (* 2 (cos (* 2 x)))   # sin(2*x) should return 2*cos(2*x)"
+# p diff('(cos (* 2 x))') # (* -2 (sin (* 2 x)))
+# p diff('(exp (* 2 x))') # (* 2 (exp (* 2 x)))   # exp(2*x) should return 2*exp(2*x)"
 
 
-config = [
-  ["constant should return 0",                       "0",                    "5"],
-  ["x should return 1",                              "1",                    "x"],
-  ["x+x should return 2",                            "2",                    "(+ x x)"],
-  ["x-x should return 0",                            "0",                    "(- x x)"],
-  ["2*x should return 2",                            "2",                    "(* x 2)"],
-  ["x/2 should return 0.5",                          "0.5",                  "(/ x 2)"],
-  ["x^2 should return 2*x",                          "(* 2 x)",              "(^ x 2)"],
-  ["cos(x) should return -1 * sin(x)",               "(* -1 (sin x))",       "(cos x)"],
-  ["sin(x) should return cos(x)",                    "(cos x)",              "(sin x)"],
-  ["exp(x) should return exp(x)",                    "(exp x)",              "(exp x)"],
-  ["ln(x) should return 1/x",                        "(/ 1 x)",              "(ln x)"],
-  ["x+(x+x) should return 3",                        "3",                    "(+ x (+ x x))"],
-  ["(x+x)-x should return 1",                        "1",                    "(- (+ x x) x)"],
-  ["2*(x+2) should return 2",                        "2",                    "(* 2 (+ x 2))"],
-  ["2/(1+x) should return -2/(1+x)^2",               "(/ -2 (^ (+ 1 x) 2))", "(/ 2 (+ 1 x))"],
-  ["cos(x+1) should return -1 * sin(x+1)",           "(* -1 (sin (+ x 1)))", "(cos (+ x 1))"],
-  ["sin(x+1) should return cos(x+1)",                "(cos (+ x 1))",        "(sin (+ x 1))"],
-  ["sin(2*x) should return 2*cos(2*x)",              "(* 2 (cos (* 2 x)))",  "(sin (* 2 x))"],
-  ["exp(2*x) should return 2*exp(2*x)",              "(* 2 (exp (* 2 x)))",  "(exp (* 2 x))"],
-]
+def recursion_cycle(exp) # упрощаем выражение
+  exp = exp.map do |ex|
+    if ex.class == Array
+      ex.any?{|e| e.class == Array} ? recursion_cycle(ex) : simplify_inner_exp(ex)
+    else
+      ex
+    end
+  end
+  simplify_inner_exp(exp)
+end
+
+def simplify_inner_exp(exp) # упрощаем внутренний массив
+  # p exp
+  if exp[1].class == Integer && exp[2].class == Integer
+    exp[0] == '**' if exp[0] == '^'
+    exp[1].send(exp[0], exp[2])
+  elsif exp[0] == '+' && (exp[1] == 0 || exp[2] == 0)
+    exp[1] == 0 ? exp[2] : exp[1]
+  elsif exp[0] == '-' && (exp[1] == 0 || exp[2] == 0)
+    exp[1] == 0 ? -exp[2] : exp[1]
+  elsif exp[0] == '*' && (exp[1] == 0 || exp[2] == 0)
+    0
+  elsif exp[0] == '^'
+    if exp[1] == 0
+      0
+    elsif exp[2] == 0 || exp[1] == 1
+      1
+    elsif exp[2] == 1
+      exp[1]
+    else
+      exp
+    end
+  elsif exp[1].class == Hash && exp[2].class == Hash && %w[+ -].include?(exp[0])  # оба с переменными
+    k = exp[1].keys[0]
+    exp = {k => exp[1][k].send(exp[0], exp[2][k])}
+  elsif exp[1..-1].any?{|e| e.class == Hash} && exp[1..-1].any?{|e| e.class != Hash} && %w[* /].include?(exp[0]) # одно с переменной
+    hh = exp[1..-1].find{|e| e.class == Hash}
+    n = exp[1..-1].find{|e| e.class != Hash}
+    k = hh.keys[0]
+    res = hh[k].send(exp[0], n.to_f)
+    res = res.to_i if res % 1 == 0
+    exp = {k => res}
+  else
+    exp
+  end
+end
+
+exp = ["/", 1, {"x"=>1}]
+p recursion_cycle(exp)
---
+++ main/e01n_irb_pry.rb
@@ -0,0 +1,69 @@
+puts '                                             irb'
+
+# irb (interactive ruby) - REPL-программа(read (R)(прочитать), evaluate (E)(выполнить), print (P), (loop - L))
+# irb(в консоли) - интерпритатор Руби сразу в консоли. Завершить работу в irb можно в любой момент нажав ctrl+c
+
+# С:\Projects\MyApp1>irb      (нажимаем enter)
+# irb(main):001:0> 2+2*2
+# => 6                        (интерпритатор возвращает(=>) нам результат действия)
+# irb(main):002:0> puts 2+2*2
+# 6                           (puts(ничего не считает только выводит) выводит результат действия)
+# => nil                      (а интерпритатор возвращает уже nil)
+# irb(main):003:0> exit       (exit команда выхода из irb  интерпритатора Руби)
+
+# http://pry.github.io/  -  альтернатива irb
+
+
+puts
+puts '                                             pry'
+
+# https://github.com/pry/pry     http://pry.github.io/
+
+# pry - “An IRB alternative and runtime developer console”. Другими словами, альтернатива уже известному нам REPL - IRB. В pry реализовано больше возможностей, чем в irb.
+
+# Проверка установлен ли pry в вашей системе:
+# > which pry
+
+# Установка:
+# $ gem install pry
+# $ gem install pry pry-doc
+
+# Запуск:
+# $ pry
+
+# Команды внутри pry:
+# > help          - выводит справку по всем возможным командам
+# > whereami -h   - получить подробную справку по любой команде: название команды и через пробел в конце добавить -h
+
+
+# (!!! Ругается на виндоус(но работает), просит почитать документацию для корректной работы на виндоус):
+#For a better Pry experience on Windows, please use ansicon:
+  #https://github.com/adoxa/ansicon
+#If you use an alternative to ansicon and don't want to see this warning again,
+#you can add "Pry.config.windows_console_warning = false" to your pryrc.
+
+
+puts
+# Важный момент в pry — конфигурация. Gem это, грубо говоря, плагин для языка(или экосистемы) руби. Но и для «плагина» pry существует свое множество плагинов:
+
+# «awesome print». Gem содержит в себе библиотеку кода, плагин для pry, плагин для irb.
+# https://github.com/awesome-print/awesome_print
+# > gem install awesome_print
+
+# (!!! ОШИБКА при подключении - системе не удается найти указанный путь)
+# Подключение(Гем awesome_print подключается к pry только один раз на вашем компьютере):
+# > cat > ~/.pryrc          # запускаем в терминале команду cat, которая считывает из стандартного ввода следующие две строки (мы должны их ввести с клавиатуры).
+# require 'awesome_print'
+# AwesomePrint.pry!
+# ^D                        # В конце мы нажимаем Ctrl+D — комбинацию, которая говорит о том, что ввод закончен (в листинге выше это обозначается как ^D)
+
+
+
+
+
+
+
+
+
+
+#
