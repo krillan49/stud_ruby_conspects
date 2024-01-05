ca672f8defa0d6fb438126383a5d0cec1b81864d
krillan <pushkinidze49@gmail.com>
2024-01-05 16:28:59 +0300

    kruk l15 16-27

--- web/15_Rails_Polym_assoc.rb
+++ web/15_Rails_Polym_assoc.rb
@@ -1,8 +1,6 @@
-puts '                                     Полиморфные ассоциации(полиморфизм)'
+puts '                                     Полиморфные ассоциации(Polymorphism)'
 
 # Создание полиморфного коммента(мой способ наугад): https://github.com/krillan49/blog2_rs
-
-# Polymorphism
 
 # Создадим приложение poly_demo(исправим ошибки таймзон, бандл апдэйт итд)
 # > rails new poly_demo
@@ -28,26 +26,23 @@ Image.comments
 # > rails g model Image url:text
 
 
-# Свяжем эти сущности.
-
+# Пропишем связи в моделях:
 # /app/models/comment.rb:
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   # При связывании с полиморфной ассоциацией надо в belongs_to добавить аргумент(любой) с окончанием able:
-  # commentable - получается: комментируемый, тоесть принадлежит комментируемому(посту, изображению итд), как бы виртуальная модель
-  # polymorphic: true - говорит о том что ассоциация полиморфная, те commentable это не какаято сущность, а хэндл(рукоятка, посредник) отвечающая за группу сущностей
-  # Хендл, рукоятка, которая существует у других сущностей.
+  # commentable - получается комментируемый, тоесть принадлежит комментируемому(посту, изображению итд), как бы виртуальной модели
+  # polymorphic: true - говорит о том что ассоциация полиморфная и соответсвенно commentable это не какаято сущность, а хэндл/рукоятка(посредник), которая существует у других сущностей, отвечающая за группу всех этих сущностей
 end
-
 # /app/models/post.rb:
 class Post < ApplicationRecord
   has_many :comments, as: :commentable
-  # as: :commentable  -  подключаем хэндл commentable к ассоциации comments
+  # as: :commentable  -  подключаем хэндл commentable к ассоциации с comments (те теперь для коммента, пост это commentable)
 end
-
 # /app/models/image.rb:
 class Image < ApplicationRecord
-  has_many :comments, as: :commentable, dependent: :destroy # dependent: :destroy - работает так же как и раньше
+  has_many :comments, as: :commentable, dependent: :destroy
+  # dependent: :destroy - при удалении изображения удалит и все принадлежащие ему комменты
 end
 
 # Далее, откроем миграцию db/migrate/20190205095251_create_comments.rb и добавим строку:
@@ -55,22 +50,20 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.text :content
-      t.references :commentable, polymorphic: true # добавляем строку, создает 2 столбца "commentable_type" и "commentable_id"
+      t.references :commentable, polymorphic: true # добавляем, создает 2 столбца "commentable_type" и "commentable_id"
       # "commentable_type" - содержит имя модели к сущности которой относится коммент, например "Post"
       # "commentable_id" - содержит id сущности(например Post) к которой принадлежит коммент
       t.timestamps
     end
   end
 end
-
-# Запускаем миграции:
 # > bundle exec rake db:migrate
 
 
 # Далее, откроем rails console:
 post = Post.create(content: 'Foo bar') # создадим пост(c id 1)
 post.comments #=> [] # все комменты данного поста
-post.comments.create(content: 'Baz Buuu Foo') # создадим комменты  #=> INSERT INTO "comments" ("content", "commentable_type", "commentable_id", "created_at", "updated_at") VALUES (?, ?, ?, ?, ?)  [["content", "Baz Buuu Foo"], ["commentable_type", "Post"], ["commentable_id", 1], ["created_at", "2023-08-15 07:53:00.708040"], ["updated_at", "2023-08-15 07:53:00.708040"]]
+post.comments.create(content: 'Baz Buuu Foo') # создадим коммент  #=> INSERT INTO "comments" ("content", "commentable_type", "commentable_id", "created_at", "updated_at") VALUES (?, ?, ?, ?, ?)  [["content", "Baz Buuu Foo"], ["commentable_type", "Post"], ["commentable_id", 1], ["created_at", "2023-08-15 07:53:00.708040"], ["updated_at", "2023-08-15 07:53:00.708040"]]
 # "commentable_type", "Post" - коммент относится к сущности модели Post
 # "commentable_id", 1 - коммент относится к сущности(Post) id которой 1
 post.comments.create(content: 'Comment 2')
@@ -103,7 +96,7 @@ image2.comments.create(content: 'Bar') #=> ... [["content", "Bar"], ["commentabl
 
 
 puts
-puts '                              Генерация полиморфнфх ассоциаций. Консерн для модели'
+puts '                              Генерация полиморфнфх ассоциаций. concern для модели'
 
 # (Для AskIt)
 
@@ -113,14 +106,14 @@ puts '                              Генерация полиморфнфх а
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :user
-  validates :body, presence: true, length: { minimum: 2 } # допишем вручную валидации
+  validates :body, presence: true, length: { minimum: 2 }  # заодно добавим валидацию
 end
 # Создалась миграция:
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.string :body
-      t.references :commentable, polymorphic: true, null: false
+      t.references :commentable, polymorphic: true, null: false # ?? тоесть миграции и БД не в курсе какие именно сущности commentable, тоесть можно будет потом добавить новые
       t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
@@ -131,7 +124,7 @@ end
 # В схеме появилось:
 create_table "comments", force: :cascade do |t|
   t.string "body"
-  t.string "commentable_type", null: false # поле c именем модели('Answer', 'Question') для привязки
+  t.string "commentable_type", null: false # поле для имени модели('Answer', 'Question') для привязки
   t.integer "commentable_id", null: false # поле с айди сущности для привязки
   t.integer "user_id", null: false
   t.datetime "created_at", null: false
@@ -156,35 +149,30 @@ end
 # 3. Пропишем маршруты для комментариев
 resources :questions do
   resources :comments, only: %i[create destroy] # добавим вложенные комменты
-  resources :answers, except: %i[new show] # тут не будем делать еще одно вложение для комментов, тк маршрут получится слишком длинный ...
+  resources :answers, except: %i[new show] # тут не будем делать еще одно вложение для комментов, тк маршрут получится слишком длинный и сложный ...
 end
-# ... вместо этого придется продублировать маршруты ответов и вложить в них комменты
+# ... вместо этого придется сделать дополнительные маршруты ответов и вложить в них комменты
 resources :answers, except: %i[new show] do
   resources :comments, only: %i[create destroy]
 end
 
 
 # 4. Добавим в application.js модуль бутстрапа collapse для выпадающих форм
-# (?? возможно придется перепройти урок 15 8-50)
+# (Для асккит урока 15, работает только через запуск st.cmd)
 
 
 # 5. Представления:
-# a. questions/show.html.erb - добавим рендер паршала comments/commentable с формой комментариев
+# a. questions/show.html.erb - добавим рендер паршала comments/commentable с формой и списком комментариев
 # б. answers/_answer.html.erb - добавим рендер паршала comments/commentable с формой комментариев (заодно добавим новую версию паршала)
 # в. comments/_commentable.html.erb - создадим директорию и паршал с формой комментариев(выпадающей) и списком всех комментариев для конкретной commentable сущности.
 # г. _comment.html.erb - создадим паршал для конкретного комментария
+# Далее сделаем выпадающими comments/_commentable.html.erb при помощи бутстрапа, для чего добавим уникальные значения айди(для каждого элемента коллекции ответов, тк для каждого будет своя форма и список комментов и для вопроса), по которому кнопка будет открывать нужный выпадающий элемент, соотв будем передавать это айди и со страниц questions/show.html.erb и answers/_answer.html.erb, где нужная форма и список комментариев и рендерятся
 
 
 # 6. Создадим новый декоратор comment_decorator.rb
 class CommentDecorator < ApplicationDecorator
   delegate_all
   decorates_association :user # задекорируем юзера, вызванного от комментария (для gravatar и name_or_email)
-
-  # def for?(commentable)
-  #   commentable = commentable.object if commentable.decorated?
-  #
-  #   commentable == self.commentable
-  # end
 end
 
 
