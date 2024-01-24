080bf3c1e5aff4c567b089e636617040df4362a9
krillan <pushkinidze49@gmail.com>
2024-01-24 18:20:09 +0300

    kruk l18 full

--- web/Rails/app/controllers/concerns/authorization.rb
+++ web/Rails/app/controllers/concerns/authorization.rb
@@ -4,11 +4,11 @@ module Authorization
   included do
     include Pundit::Authorization # Подключим Pundit
 
-    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized # обработка ошибки отсудствия прав на экшен
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized # обработка ошибки отсутствия прав на экшен
 
     private
 
-    def user_not_authorized # обработка ошибки отсудствия прав на экшен
+    def user_not_authorized # обработка ошибки отсутствия прав на экшен
       flash[:danger] = t 'global.flash.not_authorized'
       redirect_to(request.referer || root_path) # редирект на страницу с которой переходили на ошибку или корневую
     end
