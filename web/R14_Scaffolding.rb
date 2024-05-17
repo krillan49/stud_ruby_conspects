c4a25249377be87d7a4a87413768ba3283c099d1
krillan <pushkinidze49@gmail.com>
2024-05-18 02:17:45 +0300

    fdgfht

--- web/R14_Scaffolding.rb
+++ web/R14_Scaffolding.rb
@@ -11,30 +11,19 @@ puts '                                          Scaffolding'
 
 
 
-# Сделаем мини-приложение микропостинг(сильно упрощенный блог):
-# ---------------------# ---------------------
-# |  users            |# |  microposts       |
-# ---------------------# ---------------------
-# id     integer       # id        integer
-# name   string        # content   text
-# email  string        # user_id   integer      # User --1--------*-- Micropost
-# ---------------------# ---------------------
-
-
-# > rails new toy_app
-# > bundle install
+# На примере сильно упрощенного блога из 2х моделей User 1 - * Micropost:
 
 
 # Создадим модель, контроллер, экшены, маршруты, представления, тесты для сущности User
 # > rails g scaffold User name:string email:string
-# Запустим миграции(тут с правильным синтаксисом, с гемами из гемфаила а не упрощенным)
+# Запустим миграции(тут с правильным синтаксисом, с гемами из гемфаила)
 # > bundle exec rake db:migrate
 
 # Пример экшена из users_controller.rb:
 def create
   @user = User.new(user_params)
 
-  respond_to do |format| # отвечает за возврат в зависимости от запроса, если запрос из браузера то возвращает html, а если запрос от JS то возвращает json
+  respond_to do |format| # если запрос из браузера то возвращает html, а если запрос от JS то возвращает json
     if @user.save
       format.html { redirect_to user_url(@user), notice: "User was successfully created." }
       format.json { render :show, status: :created, location: @user }
@@ -54,32 +43,19 @@ root "users#index" # get '/' => 'users#index'
 # > bundle exec rake db:migrate
 
 
-# Добавим ассоциации one-to-many между пользователем и микропостом User --1--------*-- Micropost:
-# Добавим Валидацию в модель Micropost:
+# Добавим ассоциации one-to-many между пользователем и микропостом User 1 - * Micropost и валидации:
 class Micropost < ApplicationRecord
   belongs_to :user
-
   validates :content, length: { maximum: 140 }, presence: true
   validates :user_id, presence: true
 end
-# Добавим Валидацию в модель User:
 class User < ApplicationRecord
   has_many :microposts
-
   validates :name, presence: true
   validates :email, presence: true
 end
-# По умолчанию будет проверять в контроллере и затем выведет список ошибок в автоматически созданном для этого в представлении коде
+# По умолчанию выведет список ошибок в автоматически созданном для этого в представлении коде
 
-
-
-# Откроем rails console:
-first_user = User.first
-first_user.microposts                   # все посты этого юзера
-first_user.microposts.count             # обратится к БД чтоб посчитать посты(те будет нагружать БД если много использований)
-first_user.microposts.length            # прочитает то, что уже есть в памяти, без БД(не делает sql запрос)
-micropost = first_user.microposts.first # первый микропост первого пользователя
-micropost.user                          # получить пользователя микропоста
 
 
 
