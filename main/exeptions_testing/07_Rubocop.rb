95b434a5acd804822fa5540a724d17ddc9b19f92
krillan <pushkinidze49@gmail.com>
2024-07-11 10:09:37 +0300

    lfz kruk l16 21-30

---
+++ main/exeptions_testing/07_Rubocop.rb
@@ -0,0 +1,71 @@
+puts '                                         Rubocop'
+
+# https://docs.rubocop.org/rubocop/1.64/index.html
+
+# Rubocop - это гем, специальный анализатор кода, который следит за стилем кода который мы пишем, в соответсвии с существующими соглашениями по умолчанию или конфигурационным фаилом. Может сообщать о стилистических ошибках или исправлять некоторые автоматически. В итоге мы можем задать общий стиль для всех разработчиков учавствующих в проекте.
+
+
+# Установка: https://docs.rubocop.org/rubocop/1.59/installation.html
+group :development do
+  gem 'rubocop', '~> 1.64', require: false
+  # все гемы-расширения для Рубокопа можно посмотреть во вкладке меню Projects в доках
+  gem 'rubocop-performance', '~> 1.21', require: false # доп проверки на производительность кода
+  gem 'rubocop-rspec', '~> 3.0'                       # доп проверки для тестов гема rspec
+end
+# > bundle i
+
+
+# Сконфигурируем Рубокоп: https://docs.rubocop.org/rubocop/1.59/configuration.html
+# .rubocop.yml - нужно создать конфигурационный(настроечный) фаил с таким названием в корневой папке проекта (тут создадим в rspec_examples)
+# https://github.com/rubocop/rubocop/blob/master/.rubocop.yml  - пример содержания .rubocop.yml
+
+
+# Запуск проверки(в корневой директории проекта)
+# > rubocop                    # обычный
+# > bundle exec rubocop        # с версиями гемов из гемфаила
+# Пройдет все Руби фаилы проекта, в итоге выдаст список стилистических ошибок(каждая такая проверка называется 'cop') с их описаниями, а так же их колличество и сколько можно исправить автоматически
+
+
+# Автоматическое исправление тех ошибок, что можно исправить автоматически
+# > bundle exec rubocop -a        # безопасный режим исправления
+# > bundle exec rubocop -A        # агрессивное исправление, придирается к совсем мелким ошибкам и находит больше
+# После исправления выдаст отчет с описанием оставшихся ошибок и цифрами сколько исправлено из скольки
+
+
+puts '                             Ручное исправление оставшихся ошибок'
+
+# https://docs.rubocop.org/rubocop/1.64/cops.html#available-cops    - посмотреть конкретные проверки
+
+
+# Структура копа: директория фала с ошибкой, название копа, описание ошибки, место в коде выделенное ^^^:
+# app/controllers/concerns/authentication.rb:6:3: C: Metrics/BlockLength: Block has too many lines. [44/25]
+#   included do ...
+#   ^^^^^^^^^^^
+
+
+# Эта проверка нам не критична и можно ее отключить, но глобально ее отключать не хотелось бы, поэтому можем отключить ее только для конкретных строк в коде при помощи специальных комментариев, которые будут отключать проврку перед нужной строкой и включать после нее
+def remember_me
+  self.remember_token = SecureRandom.urlsafe_base64
+  # rubocop:disable Rails/SkipsModelValidations
+  update_column :remember_token_digest, digest(remember_token)
+  # rubocop:enable Rails/SkipsModelValidations
+end
+# где Rails/SkipsModelValidations - название копа
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
--- main/gems/gem_creating_fun_translations/fun_translations.gemspec
+++ main/gems/gem_creating_fun_translations/fun_translations.gemspec
@@ -30,6 +30,9 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rspec', '~> 3.12'
   spec.add_development_dependency 'simplecov', '~> 0.21'
   spec.add_development_dependency 'webmock', '~> 3.23'
+  spec.add_development_dependency 'rubocop', '~> 1.64'
+  spec.add_development_dependency 'rubocop-performance', '~> 1.21'
+  spec.add_development_dependency 'rubocop-rspec', '~> 3.0'
 
   # метаданные
   spec.metadata = {
