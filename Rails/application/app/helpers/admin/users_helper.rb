8bf7099cc6d36c20ba4af5a908abbf794c147357
krillan <pushkinidze49@gmail.com>
2024-09-17 10:22:32 +0300

    fhtetsgktgf

--- Rails/application/app/helpers/admin/users_helper.rb
+++ Rails/application/app/helpers/admin/users_helper.rb
@@ -3,7 +3,7 @@ module Admin
     # для наполнения селектора в admin/users/_form.html.erb ролями
     def user_roles
       User.roles.keys.map do |role|
-        # User.roles - обращвемся к модели и получаем от enum хэш ролей { basic: 0, moderator: 1, admin: 2 }
+        # roles - обращвемся к методу модели и получаем от enum хэш ролей { basic: 0, moderator: 1, admin: 2 }
         # role - соответсвенно содержит ключ :basic или :moderator или :admin
         [t(role, scope: 'global.user.roles'), role] # для каждой роли генерим массив из 2х элементов
         # t(role, scope: 'global.user.roles') - будет отображаться в селекторе для пользователя
