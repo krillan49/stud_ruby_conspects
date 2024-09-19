0c4239840836454b029692c4e07d2e491baf8a37
krillan <pushkinidze49@gmail.com>
2024-09-19 11:38:07 +0300

    uytgfdlkjnbvhf

--- Rails/application/app/controllers/concerns/authorization.rb
+++ Rails/application/app/controllers/concerns/authorization.rb
@@ -8,7 +8,7 @@ module Authorization
 
     private
 
-    def user_not_authorized # обработка ошибки отсутствия прав на экшен
+    def user_not_authorized # метод обработчик ошибки отсутствия прав на экшен
       flash[:danger] = t 'global.flash.not_authorized'
       redirect_to(request.referer || root_path) # редирект на страницу с которой переходили на ошибку или на корневую
     end
