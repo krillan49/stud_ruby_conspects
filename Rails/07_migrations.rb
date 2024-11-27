bb57d5c20391e13eb8dff0ad6137bb0c623ee68b
krillan <pushkinidze49@gmail.com>
2024-11-27 12:32:45 +0300

    tyuhjnbghfg

--- Rails/07_migrations.rb
+++ Rails/07_migrations.rb
@@ -1,4 +1,4 @@
-puts '                                       Миграции(AR)'
+puts '                                         Миграции(AR)'
 
 # https://api.rubyonrails.org/classes/ActiveRecord/Migration.html
 
@@ -9,9 +9,11 @@ puts '                                       Миграции(AR)'
 
 
 
-puts '                                    change_column_null'
+puts '                                       change_column_null'
 
-# Сделаем миграцию которая запретит значения NULL в полях таблиц questions и answers(валидация на уровне БД)
+# change_column_null - метод, который вызывается в методе change, в классе миграции, он назначает/снимает ограничение NOT NULL в заданную переданным в него параметром колонку/поле таблицы (валидация на уровне БД). Применяется к одному полю одной таблицы, для другого поля нужно вызвать метод еще раз. При выполнении миграции с применением этого метода никакие данные потеряны не будут
+
+# Сделаем миграцию которая запретит значения NULL в полях таблиц questions и answers
 
 # > rails g migration add_missing_null_checks
 
@@ -19,10 +21,9 @@ puts '                                    change_column_null'
 class AddMissingNullChecks < ActiveRecord::Migration[7.0]
   def change
     change_column_null :questions, :title, false
-    # change_column_null - метод для того чтобы запретить/разрешить NULL в колонке
     # :questions - название таблицы
     # :title     - название поля
-    # false      - означает, что в данной колонке значения NULL быть не может
+    # false      - означает, что в данной колонке значения NULL быть не может, тоесть применено ограничение NOT NULL
     change_column_null :questions, :body, false
     change_column_null :answers, :body, false
   end
@@ -33,64 +34,62 @@ end
 # Теперь в схеме эти колонки получили опцию null: false
 t.string "title", null: false
 
-# При такой миграции никакие данные потеряны не будут
-
 
 
 puts '                                      add_column, add_index'
 
-# Пример добавления столбцов и индексов есть в Девайс
+# add_column - метод, который вызывается в методе change, в классе миграции, для создания нового столбца в таблице. Через один вызов метода можно добавить только однин новый столбец, для другого нужно вызывать метод еще раз
 
-def change
-  add_column :users, :username, :string
-  # add_column - метод для создания столбца(одного отдельного, для другого нужно писать еще раз add_column ...)
-  # :users    - имя таблицы в которую добавим новый столбец
-  # :username - имя нового столбца
-  # :string   - тип данных нового столбца
-  # (4м аргументом можно поставить значение по умолчанию, пример - default: false)
+# add_index - метод, который вызывается в методе change, в классе миграции, для создания мндекса (?? с ограничениями ??). Индексы можно добавить и в отдельной миграции, например назвать ее add_index или в одной миграции со столбцом или при создании таблицы.
+# Индекс - это когда для нашей таблицы создается доп таблица, с указателями для более быстрого выбора записей по ключу(полю). Для полей с индексами увеличивается время вставки, но уменьшается время выборки по определённому полю.
 
-  # По умолчанию можно будет вставить в этот столбец неуникальный username(те может быть 2+ одинаковых). Чтобы это исправить создадим на уровне БД уникальный индекс, который означает, что в этот столбец можно будет вставить только уникальные значения, если будем вставлять неуникальные, то ошибка будет уже на уровне БД.
-  # Индекс - это когда для нашей таблицы создается доп таблица, с указателями для более быстрого выбора записей по ключу(полю). Для полей с индексами увеличивается время вставки, но уменьшается время выборки по определённому полю.
-  add_index :users, :username, unique: true
-  # Имя индексу создается автоматически
-  # (Индексы можно добавить и в отдельной миграции, например назвать ее add_index)
+# (Пример добавления столбцов и индексов есть в разделе Девайс)
 
-  # Добавим еще колонку:
-  add_column :users, :password_reset_token, :string
-end
+# > rails g migration add_username_and_token
 
-
-
-puts '                                           add_reference'
-
-# Свяжем сущности User и Article как 1 - * уже после их создания, добавив поле user_id и индекс в таблицу articles
-
-# Добавим столбец с user_id отдельной миграцией
-# > rails g migration add_fk_article_to_user
-# /db/migrate/20230804075512_add_fk_article_to_user.rb
-class AddFkArticleToUser < ActiveRecord::Migration[7.0]
+# /db/migrate/20190129063426_add_username_and_token.rb
+class AddUsernameAndToken < ActiveRecord::Migration[7.0]
   def change
-    add_reference :articles, :user # foreign key ??
-    # В схеме миграйций в таблице articles появятся:
-    # t.integer "user_id"
-    # t.index ["user_id"], name: "index_articles_on_user_id"
+    add_column :users, :username, :string
+    # :users    - имя таблицы в которую добавим новый столбец
+    # :username - имя нового столбца
+    # :string   - тип данных нового столбца
+    # (4м аргументом можно поставить значение по умолчанию, пример - default: false)
+
+    add_column :users, :password_reset_token, :string # добавим еще колонку
+
+    add_index :users, :username, unique: true
+    # :users        - имя таблицы
+    # :username    - имя столбца на который будет поставлен индекс
+    # unique: true - доп опция с ограничением UNIQUE для данного столбца
+    # Имя индексу создается автоматически
   end
 end
 # > rake db:migrate
 
-# /models/user.rb
-class User < ApplicationRecord
-  has_many :articles # добавим
-end
 
-# /models/article.rb:
-class Article < ApplicationRecord
-  belongs_to :user, optional: true, required: true # добавим
-  # optional: true - если это не добавить то при использовании Rails 5.1 и выше создание новой статьи и тесты этого свойства выдадут ошибку "User must exist". Видимо изза того что в старых статьях созданных до связывания в колонке user_id NULL
-  # required: true - если не добавить возникнут ошибки с rspec тестирыванием этой ассоциации
-end
 
-# Маршруты можно тоже поменять и вложить статьи в юзеров если надо
+puts '                                         add_reference'
+
+# add_reference - метод, который вызывается в методе change, в классе миграции, создает поле вторичного ключа под именем сущьность_id и связывает его с первичным ключем указанной в параметрах таблицы. Удобно чтобы создавать связи для уже созданных таблиц
+
+# Свяжем сущности User 1 - * Article, добавив поле user_id и индекс в таблицу articles
+
+# > rails g migration add_fk_article_to_user
+
+# /db/migrate/20230804075512_add_fk_article_to_user.rb
+class AddFkArticleToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :articles, :user
+    # :articles - таблица в которой будет создано поле вторичного ключа user_id
+    # :user     - таблица на первичный ключ (id) которой будет ссылаться user_id
+  end
+end
+# > rake db:migrate
+
+# В схеме миграйций в таблице articles появятся:
+t.integer "user_id"
+t.index ["user_id"], name: "index_articles_on_user_id"
 
 
 
