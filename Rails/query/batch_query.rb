19cbb37a8dbb544ec73855d71fa70ea75c854ea7
krillan <Pushkinidze49@gmail.com>
2025-05-23 19:04:15 +0300

    gfhgfhgfhggh

--- Rails/query/batch_query.rb
+++ Rails/query/batch_query.rb
@@ -1,18 +1,22 @@
 puts '                         Запросы вставки и обновления париями вместо одиночных'
 
-# https://api.rubyonrails.org/classes/ActiveRecord/Relation.html#method-i-insert_all
-
 # https://www.postgresql.org/docs/current/sql-merge.html
 
-# Имеет смысл вставлять записи батчами. Рельсы уже предоставляют из коробки метод insert_all. Кроме того, можно рассмотреть написание SQL запроса, если требуется более сложная обработка, например, удаление/замена/вставка удобно делается с помощью MERGE
+# Имеет смысл вставлять записи батчами, что значительно улучшит производительность по сравнению с вставкой по одной записи. Использование в сочетании с массивами позволяет значительно ускорить процесс.
 
-# insert_all - этот метод (метод Rails) позволяет вставить (только вставить, а не обновить) несколько записей за один SQL-запрос, что значительно улучшит производительность по сравнению с вставкой по одной записи. Использование в сочетании с массивами позволяет значительно ускорить процесс. Это стандартный и эффективный способ для массовой вставки в Rails, и он подходит для большинства сценариев.
+# Рельсы уже предоставляют из коробки методы insert_all и upsert_all для этого.
 
 # Если нужно обновлять существующие записи или выполнять более сложные операции вставки/обновления (с условием?), можно использовать SQL-оператор MERGE (или эквивалентный ON CONFLICT для PostgreSQL), который позволяет вставлять или обновлять записи в одном запросе
 
 
 
 puts '                                      insert_all и upsert_all'
+
+# https://api.rubyonrails.org/classes/ActiveRecord/Relation.html#method-i-insert_all
+
+# insert_all и upsert_all - методы, которые добавлены в добавленный в ActiveRecord 6.0. Это стандартные и эффективные способы для массовой вставки и оновления записей в Rails, и они подходят для большинства сценариев.
+
+# Поддерживаемые СУБД только: PostgreSQL, SQLite, MySQL 8+ (Старые версии MySQL и другие базы не поддерживают). При этом в PostgreSQL наиболее мощная и полная поддержка этих фич.
 
 # Оба метода не запускают валидации или callbacks
 
@@ -25,9 +29,7 @@ puts '                                      insert_all и upsert_all'
 
 puts '                                             insert_all'
 
-# insert_all - метод, добавленный в ActiveRecord 6.0, который позволяет вставить много записей за один SQL-запрос, минуя обычные валидации, callbacks и другие хуки. Очень быстрый способ загрузить много данных (например, из Excel или API)
-
-# Поддерживаемые СУБД: PostgreSQL, SQLite, MySQL 8+ (Старые версии MySQL и другие базы не поддерживают)
+# insert_all - метод позволяет вставить (только вставить, а не обновить) много записей за один SQL-запрос, минуя обычные валидации, callbacks и другие хуки. Очень быстрый способ загрузить много данных (например, из Excel или API)
 
 # insert_all принимает массив хэшей, где каждый хэш это запись для вставки
 # Все хэши должны содержать одинаковые ключи
@@ -56,13 +58,11 @@ Purchase.insert_all([
 # Когда записи содержат ассоциации, вложенные модели или файлы (ActiveStorage, has_many, итд)
 
 
-# SQL-запросы, которые ActiveRecord генерирует при использовании insert_all в PostgreSQL (наиболее мощной и полной поддержкой этих фич):
-
+# SQL-запросы, которые ActiveRecord генерирует при использовании insert_all в PostgreSQL:
 Purchase.insert_all([
   { barcode_id: 1, quantity: 10, created_at: Time.current, updated_at: Time.current },
   { barcode_id: 2, quantity: 20, created_at: Time.current, updated_at: Time.current }
 ])
-
 # SQL, который сгенерирует Rails (PostgreSQL) это один простой INSERT INTO ... VALUES ... без обработки конфликтов (будет ошибка, если нарушен уникальный индекс):
 <<-SQL
 INSERT INTO "purchases" ("barcode_id", "quantity", "created_at", "updated_at")
@@ -75,13 +75,9 @@ SQL
 
 puts '                                           upsert_all'
 
-# upsert_all - метод, добавленный в ActiveRecord 6.0 для PostgreSQL, SQLite и некоторых других баз данных. Он позволяет вставлять или обновлять сразу много записей за один SQL-запрос, используя механизм PostgreSQL ON CONFLICT. Работает так же как и insert_all, но с логикой: "если конфликт по уникальному ключу — обнови":
+# upsert_all - метод позволяет вставлять или обновлять сразу много записей отправляя один SQL-запрос, а не по одной (.update в цикле), используя механизм PostgreSQL ON CONFLICT, что в десятки и сотни раз быстрее при большом объеме данных. Работает так же как и insert_all, но с логикой: "если есть конфликт по уникальному ключу то обнови":
 # а) Если записи не существуют (по уникальному ключу) - они будут вставлены
 # б) Если записи уже существуют                       - они будут обновлены
-
-# Вместо того чтобы обновлять записи по одной (.update в цикле), upsert_all отправляет один SQL-запрос, что в десятки и сотни раз быстрее при большом объеме данных
-
-# Поддерживается только PostgreSQL, SQLite и MySQL 8+
 
 # Синтаксис:
 Model.upsert_all(array_of_hashes, unique_by: :index_or_column, update_only: [:fields])
@@ -92,7 +88,7 @@ Model.upsert_all(array_of_hashes, unique_by: :index_or_column, update_only: [:fi
 # Особенности:
 # Нужен уникальный индекс по полю, указанному в unique_by
 # Если не указать unique_by:, ActiveRecord попытается сам найти уникальный индекс (может не сработать, если их несколько)
-# При upsert_all Rails не обновляет created_at, а только updated_at (только если его явно передавать)
+# При upsert_all Rails не обновляет created_at, а только updated_at (и только если его явно передавать)
 
 # Пример
 Purchase.upsert_all([
@@ -102,8 +98,7 @@ Purchase.upsert_all([
 # Если записи с ID 1 и 2 уже существуют - они будут обновлены. Если нет - будут созданы
 
 
-# SQL-запросы, которые ActiveRecord генерирует при использовании upsert_all в PostgreSQL (наиболее мощной и полной поддержкой этих фич)
-
+# SQL-запросы, которые ActiveRecord генерирует при использовании upsert_all в PostgreSQL:
 Purchase.upsert_all([
   { barcode_id: 1, quantity: 15, updated_at: Time.current },
   { barcode_id: 2, quantity: 25, updated_at: Time.current }
@@ -133,10 +128,8 @@ Purchase.upsert_all(
     { barcode_id: 2, purchase_date: Date.new(2025, 5, 21), quantity: 20, updated_at: Time.current }
   ],
   unique_by: :index_purchases_on_barcode_and_date
-)
-# Здесь :unique_by - это имя уникального индекса, а не просто массив колонок.
-
-# SQL (PostgreSQL)
+) # :unique_by - это имя уникального индекса, а не просто массив колонок
+# SQL (PostgreSQL):
 <<-SQL
 INSERT INTO "purchases" ("barcode_id", "purchase_date", "quantity", "updated_at")
 VALUES
